0a6f5c6301ceab2cc5e349e6d80f65b880cb9581
omid.esmaeeli <omidesmaeeli4@gmail.com>
2019-07-02 02:43:31 +0430

    Complete SignIn

--- contact_backend/urls.py
+++ contact_backend/urls.py
@@ -14,8 +14,28 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+from rest_framework import routers
+from django.conf import settings
+from django.conf.urls.static import static
+
+from contacts.views import UserViewSet, ContactViewSet, AddContactForUser, UserConatctView, SignupView
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'contacts', ContactViewSet)
+router.register(r'user/contacts', UserConatctView, basename='user-contacts-list')
+# router.register(r'signup', )
 
 urlpatterns = [
+    re_path(r'^', include(router.urls)),
     path('admin/', admin.site.urls),
+    re_path(r'^api-auth/', include('rest_framework.urls')),
+    re_path(r'^contact/add/', AddContactForUser.as_view(), name='c'),
+    re_path(r'^signup/', SignupView.as_view(), name='signup'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
